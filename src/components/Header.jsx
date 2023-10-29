9eac267db8614c5bc3b51ed98cb3663182cecbff
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-29 22:58:32 +0100

    Add header banner

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderBanner from './HeaderBanner'
+
+const Header = () => {
+  return (
+    <header>
+      <HeaderBanner />
+    </header>
+  )
+}
+
+export default Header
