9fa23d17b5b50f10265f7cae2b064c1ade37d480
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-10-30 14:03:46 +0100

    define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary1: "#363738",
+        secondary1: "#FEFAF1",
+        text: "#FAFAFA",
+        
+      }
+    },
   },
   plugins: [],
 }
