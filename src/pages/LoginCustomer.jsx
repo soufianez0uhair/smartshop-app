55298a66de824ed94665bdfde4511c2ac4c8b136
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2023-11-03 19:37:56 +0100

    complete login customer page

--- src/pages/LoginCustomer.jsx
+++ src/pages/LoginCustomer.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import LoginCustomerForm from "../components/Login/LoginCustomerForm"
 
 const LoginCustomer = () => {
   return (
-    <div>LoginCustomer</div>
+    <div className='flex justify-center items-center h-[calc(100vh-131.5px)] min-w-full'>
+      <LoginCustomerForm />
+    </div>
   )
 }
 
